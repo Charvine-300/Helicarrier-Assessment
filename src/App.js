5a8419b3a4a901c9da7b0952e6a79d0609eefc86
Ezenwekwe Chidera <marvinezenwekwe@gmail.com>
2022-07-09 19:57:57 +0100

    List of 40 characters for app fetched from GraphQL API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
 import './App.css';
+import { useQuery, gql } from '@apollo/client';
+
+//Creating an array that contains the list of IDs of the characters
+const IDS = [...Array(300).keys()].slice(1);
+
+//Creating batches of characters with different dates
+const FirstID = IDS.slice(0, 5);
+const SecondID = IDS.slice(70, 75);
+const ThirdID = IDS.slice(90, 95);
+const FourthID = IDS.slice(110, 115);
+const FifthID = IDS.slice(150, 155);
+const SixthID = IDS.slice(170, 175);
+const SeventhID = IDS.slice(200, 205);
+const EighthID = IDS.slice(270, 275);
+
+//Concatenating all batches of IDs into one array for the query
+const allIDS = [
+  ...FirstID, 
+  ...SecondID, 
+  ...ThirdID, 
+  ...FourthID, 
+  ...FifthID, 
+  ...SixthID, 
+  ...SeventhID, 
+  ...EighthID
+];
+
+
+//Query for fetch a list of characters from id 1 - 40
+const getCharacters = gql`
+  query getCharacters {
+    characters: charactersByIds(ids: [${allIDS}]) {
+      id
+      name
+      status
+      species
+      type
+      image
+      created
+      location {
+        id
+        name
+        type
+        dimension
+      }
+    }
+  }
+`;
+
+
 
 function App() {
+  const { loading, error, data } = useQuery(getCharacters);
+
+  if (loading) return <p> Loading... </p>;
+  if (error) return <p> Error :( </p>;
+
+  console.log(data.characters);
+  const characterList = data.characters;
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1> Rick and Morty </h1>
+        <p> Characters </p>
+        <input type="text" id="search-term" placeholder='Search Character...' />
       </header>
-    </div>
+      <div>
+        {characterList.map(character => (
+          <div className='App-header' key={character.id}>
+            <img src={character.image} alt={character.name} />
+            <div>
+              <h3> {character.name} </h3>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
